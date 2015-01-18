9baf4246ea9cfff8b1e4a5677d8a256002e6377a
Chad Schone <chadsch1@gmail.com>
2015-01-17 21:23:02 -0700

    Working and published on SmartThings. To Do: Would like to force the switch on in case the physical switch ever turns the device off. Need to figure out how to trigger the smartApp without a device event.

--- smartApp/SumpPumpPowerMonitor.groovy
+++ smartApp/SumpPumpPowerMonitor.groovy
@@ -1,45 +1,44 @@
 /**
- *  Sump Pump Power Monitor
+ *	Sump Pump Power Monitor
  *
- *  Copyright 2014 Chad Schone
+ *	Copyright 2014 Chad Schone
  *
- *  Based on https://github.com/sudarkoff/smarttings/blob/develop/BetterLaundryMonitor.groovy
- *  by sudarkoff
+ *	Based on https://github.com/sudarkoff/smarttings/blob/develop/BetterLaundryMonitor.groovy
+ *	by sudarkoff
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
  *
  */
  
 definition(
-    name: "Sump Pump Power Monitor",
-    namespace: "skismatik",
-    author: "Chad Schone",
-    description: "Use an power meter to monitor sump pump activity.",
-    category: "Safety & Security",
-    iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet.png",
-    iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet@3x.png")
-
+	name: "Sump Pump Power Monitor",
+	namespace: "cschone",
+	author: "Chad Schone",
+	description: "Use an power meter to monitor sump pump activity.",
+	category: "Safety & Security",
+	iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet.png",
+	iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartthings-device-icons/alarm/water/wet@3x.png")
 
 preferences {
-  section ("When this device starts drawing power") {
-    input "meter", "capability.powerMeter", multiple: false, required: true
-  }
+	section ("When this device starts drawing power") {
+		input "meter", "capability.powerMeter", multiple: false, required: true
+	}
 
-  section (title: "Notification method") {
-    input "sendPushMessage", "bool", title: "Send a push notification?"
-  }
-  
-  section (title: "Notification method") {
-    input "phone", "phone", title: "Send a text message to:", required: false
-  }
+	section (title: "Notification method") {
+		input "sendPushMessage", "bool", title: "Send a push notification?"
+	}
+	
+	section (title: "Notification method") {
+		input "phone", "phone", title: "Send a text message to:", required: false
+	}
 }
 
 def installed() {
@@ -56,38 +55,70 @@ def updated() {
 }
 
 def initialize() {
-	subscribe(meter, "power", handler)
+	subscribe(meter, "energy", handler)
+    subscribe(meter, "power", handler)
+	state.cycleOn = false;
+	meter.reset();
+	state.lastEnergy = 0
+	log.trace "initialize"
+	log.debug "lastEnergy: ${state.lastEnergy}"
+    log.trace "Forced switch on."
+    meter.on()
 }
 
 def handler(evt) {
-	def latestPower = meter.currentValue("power")
-    log.trace "Sump Pump Power Monitor: ${latestPower}W"
+	log.trace "handler"
+	def currentEnergy = meter.currentValue("energy")
+    def currentPower = meter.currentValue("power")
+    //def currentState = meter.currentValue("switch")
     
-    if (!state.cycleOn && latestPower > 2) {
-    	state.cycleOn = true
-        tate.cycleStart = now()
-        log.trace "Cycle started."
-        
-        // If the sump pump starts drawing power, send notification.
-        def message = "Check your sump pump!"
-    	send()
-  	} else if (state.cycleOn && latestPower == 0) {
-    	state.cycleOn = false
-    	state.cycleEnd = now()
-    	duration = state.cycleEnd - state.cycleStart
-    	log.trace "Cycle ended after ${duration} minutes."
-    }
+	log.trace "Current Energy: ${currentEnergy}"
+    log.trace "Current Power: ${currentPower}"
+    //log.trace "Current Power: ${currentState}"
+    
+    def isRunning = (currentEnergy > state.lastEnergy) || (currentPower > 0)
+	
+	if (!state.cycleOn && isRunning) {
+		// If the sump pump starts drawing energy, send notification.
+		state.cycleOn = true
+		def message = "Check your sump pump!"
+		log.trace "${message}"
+		send(message)
+	} else if (state.cycleOn && isRunning) {
+		// If the sump pump continues drawing energy,
+		// send more notifications.
+		def message = "Your sump pump is still running!"
+		log.trace "${message}"
+        // this is probably overkill
+		//send(message)
+	} else if (state.cycleOn && !isRunning) {
+		// If the sump pump stops drawing power, send notification.
+		state.cycleOn = false
+		def message = "Your sump pump stopped running!"
+		log.trace "${message}"
+		send(message)
+	} else {
+    	// this should not happen
+		log.trace "No activity."
+	}
+	state.lastEnergy = currentEnergy;
+    
+    // negate physical on/off switch
+    // If using this device as a monitor it shold never be off
+    //if(currentState == "off") {
+    //	log.trace "Forced switch on."
+    //	meter.on()
+    //}
 }
 
-private send() {
-	def msg = "Your sump pump is running!"
-    if (sendPushMessage) {
-        sendPush(msg)
-    }
+private send(msg) {
+	if (sendPushMessage) {
+		sendPush(msg)
+	}
 
-    if (phone) {
-        sendSms(phone, msg)
-    }
+	if (phone) {
+		sendSms(phone, msg)
+	}
 
-    log.debug msg
+	log.debug msg
 }
